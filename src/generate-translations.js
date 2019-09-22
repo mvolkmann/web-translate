812a827ca53a1dd3c715c277756aebc9063148df
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-09-22 07:15:30 -0500

    added svelte to file extensions; bumped to 1.0.0

--- src/generate-translations.js
+++ src/generate-translations.js
@@ -141,7 +141,7 @@ function getSourceFiles(dirPath) {
   }
 }
 
-const sourceExtensions = ['js', 'jsx', 'ts', 'tsx', 'vue'];
+const sourceExtensions = ['js', 'jsx', 'svelte', 'ts', 'tsx', 'vue'];
 export function isSourceFile(file) {
   const index = file.lastIndexOf('.');
   const extension = file.substring(index + 1);
